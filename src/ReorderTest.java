839b639f007332de588c45edd50e84c08d127579
abandondR <yukinghe2020@gmail.com>
2020-04-01 23:26:00 +0800

    initial project

---
+++ src/ReorderTest.java
@@ -0,0 +1,61 @@
+/**
+ * Java中指令重排序问题证明
+ *
+ * 代码来源：https://tech.meituan.com/2014/09/23/java-memory-reordering.html
+ * @author Yu
+ * @since 2020/4/1
+ */
+public class ReorderTest {
+
+    static int x = 0, y = 0;
+    static int a = 0, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        for (; ; ) {
+            i++;
+            x = 0;
+            y = 0;
+            a = 0;
+            b = 0;
+            Thread one = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    //由于线程one先启动，下面这句话让它等一等线程two. 读着可根据自己电脑的实际性能适当调整等待时间.
+                    //shortWait(100000);
+                    a = 1;
+                    x = b;
+                }
+            });
+
+            Thread other = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    b = 1;
+                    y = a;
+                }
+            });
+            one.start();
+            other.start();
+            one.join();
+            other.join();
+            String result = "第" + i + "次 (" + x + "," + y + "）";
+            if (x == 0 && y == 0) {
+                System.err.println(result);
+                break;
+            } else {
+//                System.out.println(result);
+            }
+        }
+    }
+
+
+    public static void shortWait(long interval) {
+        long start = System.nanoTime();
+        long end;
+        do {
+            end = System.nanoTime();
+        } while (start + interval >= end);
+    }
+}
+
